dd6b93b5e1d9d24ef841d9f05121a4f161b614db
Luke Bresler <luke@realpixel.co.za>
2026-01-15 18:00:06 +0200

    Initial Commit

---
+++ Puzzle Box/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Puzzle Box
+//
+//  Created by Luke Bresler on 2026/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Puzzle Box/Puzzle_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle_BoxApp.swift
+//  Puzzle Box
+//
+//  Created by Luke Bresler on 2026/01/15.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
